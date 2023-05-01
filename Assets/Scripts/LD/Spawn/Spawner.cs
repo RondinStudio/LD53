db0ee46fd78b7cf1bd113e37409b66ffb73cbc41
buloz <lbizeray30@gmail.com>
2023-05-01 15:50:35 +0200

    [obstacle_spawn] SpawnPoint object implementation + demo prefab moving obstacle + timescore correction + demo spawners

--- Assets/Scripts/LD/Spawn/Spawner.cs
+++ Assets/Scripts/LD/Spawn/Spawner.cs
@@ -8,7 +8,7 @@ public class Spawner : MonoBehaviour
 {
     public List<GameObject> Prefab;
 
-    public List<Transform> SpawnPointsList = new ();
+    public List<GameObject> SpawnPointsList = new ();
 
     public int MaxElements = 0;
 
@@ -27,9 +27,9 @@ public class Spawner : MonoBehaviour
                 try
                 {
                     GameObject randomPrefab = Prefab[UnityEngine.Random.Range(0, Prefab.Count - 1)];
-                    Transform randomSpawnPoint = SpawnPointsList[UnityEngine.Random.Range(0, SpawnPointsList.Count - 1)];
+                    GameObject randomSpawnPoint = SpawnPointsList[UnityEngine.Random.Range(0, SpawnPointsList.Count - 1)];
 
-                    GameObject newObject = Instantiate(randomPrefab, randomSpawnPoint);
+                    GameObject newObject = Instantiate(randomPrefab, randomSpawnPoint.transform);
                     GameObjects.Add(newObject);
                     OnGameObjectsListChange?.Invoke(this, new GameObjectCollectionChangedEventArgs(newObject, randomSpawnPoint));
                 }
